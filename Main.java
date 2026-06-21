879e5b9cf0b4bf4bb4cce118f5ca8cad8b82d31b
faheeemm <faheemahmedhyd82@gmail.com>
2026-06-21 16:43:59 +0530

    added Create nakedprnt core utility

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("helloWorld");
+        Sys.prnt("helloWorld");
     }
 }
---
+++ Sys.java
@@ -0,0 +1,5 @@
+public class Sys {
+    public static void prnt(String x) {
+        System.out.println(x); // custom method underthehood still uses the real println
+    }
+}
